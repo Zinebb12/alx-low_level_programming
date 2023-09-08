ccdbc6778090ac3ec79ff13836ad6cdfe497bf23
Zinebb12 <zinebzaid@gmail.com>
2023-09-07 19:26:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a C program function
+ * Return: success
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
