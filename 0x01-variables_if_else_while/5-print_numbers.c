0825ec25be418713e79c9c404e3b143e0cbe57bf
Zinebb12 <zinebzaid@gmail.com>
2023-09-08 20:00:02 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	printf("%d", num);
+	num++;
+	}
+	printf("\n");
+	return (0);
+}
