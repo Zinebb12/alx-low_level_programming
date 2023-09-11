26a0636428c7da99f6110fcc00c0b9ff23d86cda
Zinebb12 <zinebzaid@gmail.com>
2023-09-10 17:52:34 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+	putchar(ch);
+	ch--;
+	}
+	putchar('\n');
+	return (0);
+}
