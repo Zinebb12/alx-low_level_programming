7cece28a6daaf0099215ed11d6c013a798643fe7
Zinebb12 <zinebzaid@gmail.com>
2023-09-12 18:26:25 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	_putchar(str[ch]);
+
+	_putchar('\n');
+	return (0);
+
+}
