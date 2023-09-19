b27581484b7cd372597b5ebdc9ae9ef680520e11
Zinebb12 <zinebzaid@gmail.com>
2023-09-18 17:33:43 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+*_puts - is a function that take pointer as parameter
+*@str: is the name of the variable
+*Return: print a string followed by new line
+*/
+
+void _puts(char *str);
+{
+	while (str != '\0')
+	{
+	_putchar(str + 0);
+	str++;
+	}
+	_putchar('\n');
+}
