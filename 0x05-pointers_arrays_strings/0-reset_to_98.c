eadc8f1429f57636e46c05a7f2177ea0eb07383c
Zinebb12 <zinebzaid@gmail.com>
2023-09-18 17:05:45 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the variable
+ * @n: intger value
+ * Return: take a poiter that point to inetger an assigne it to number 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
