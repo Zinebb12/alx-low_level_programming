e976b9544306af4f75ee010f66658876d4e88f5b
Zinebb12 <zinebzaid@gmail.com>
2023-09-22 13:25:43 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,22 @@
 #include "main.h"
-
 /**
- * _strcmp - compares two strings
- * @s1: first string
- * @s2: second string
+ * _strcmp - compare string values
+ * @s1: string for comparaison
+ * @s2: the other string for comparaison
  *
- * Return: s1[z] - s2[z]
+ * Return: s1[i] - s2[i]
  */
-
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-	int z = 0
+	int i = 0;
 
-	while (s1[z] != '\0' && s2[z] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[z] != s2[z])
+		if (s1[i] != s2[i])
 		{
-			return (s1[z] - s2[z]);
+			return (s1[i] - s2[i]);
 		}
-		z++;
+	i++;
 	}
 	return (0);
 }
-
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H 
 #define MAIN_H 
 
+#include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
