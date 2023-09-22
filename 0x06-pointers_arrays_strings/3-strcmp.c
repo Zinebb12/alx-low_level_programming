8a132e13626230bb7664c07edd23ea254247ff4a
Zinebb12 <zinebzaid@gmail.com>
2023-09-21 18:19:12 -0700

    rev array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,15 @@ int _strcmp(char *s1, char *s2);
 {
 	int z;
 
-	for (z = 0; s1[z] && s2[z] && s1[z] == s2[z]; z++)
-		;
-	return (s1[z] - s2[z]);
+	z = 0;
+	while (s1[z] != '\0' && s2[z] != '\0')
+	{
+		if (s1[z] != s2[z])
+		{
+			return (s1[z] - s2[z]);
+		}
+		z++;
+	}
+	return (0);
 }
+
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of the integers
+ * @a: array
+ * @n: number of the elements o the  array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int z;
+	int b;
+
+	for (z = 0; i < z--; i++)
+	{
+		b = a[z];
+		a[z] = a[n];
+		a[n] = b;
+	}
+}
+
