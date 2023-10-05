95824653823a9371eea178d543f0c165b4f51cea
Zinebb12 <zinebzaid@gmail.com>
2023-10-04 18:31:40 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - create 2 dimensional arrays of ints
+ * @width: width
+ * @height: height
+ * Return: a pointer to the 2 dimensional array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int *) * height);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+
+		if (array[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(array[i]);
+
+			free(array);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+
+	return (array);
+}
+
