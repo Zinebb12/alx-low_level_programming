9c2b4a8431bbe5b6787ee46030cfe45bf49900d3
Zinebb12 <zinebzaid@gmail.com>
2023-10-19 12:32:16 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_first - function that print str before main
+ * Return: void
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
