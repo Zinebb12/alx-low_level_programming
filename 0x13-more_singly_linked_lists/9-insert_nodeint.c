5002f447d470f28dc304f5e9983b672f78839753
Zinebb12 <zinebzaid@gmail.com>
2023-10-24 18:11:03 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,33 +8,34 @@
  * @n: Data of the new node.
  * Return: Address of the new node, or NULL if it fails
  **/
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_nd, *last;
-	unsigned int i = 0;
+	listint_t *new, *last = *head;
+	unsigned int node;
 
-	if (*head == NULL && idx != 0)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-	if (idx != 0)
-	{
-	last = *head;
-	for (; i < idx - 1 && temp != NULL; i++)
-		last = last->next;
-	if (last == NULL)
-		return (NULL);
-	}
-	new_nd = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_nd->n = n;
+
+	new->n = n;
+
 	if (idx == 0)
 	{
-		new_nd->next = *head;
-		*head = new_nd;
-		return (new_nd);
+		new->next = last;
+		*head = new;
+		return (new);
 	}
-	new_nd->next = last->next;
-	last->next = new_nd;
-	return (new_nd);
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (last == NULL || last->next == NULL)
+			return (NULL);
+
+		last = last->next;
+	}
+
+	new->next = last->next;
+	last->next = new;
+
+	return (new);
 }
