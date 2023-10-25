cb342eb4d2df801a7fd8bfa498dd43b826519f3f
Zinebb12 <zinebzaid@gmail.com>
2023-10-24 17:30:19 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint2 - function that frees a listint_t list.
- * @head: ptr to the adresses of the head
+ * @head: pointer to the list
  * Return: void
  */
 void free_listint2(listint_t **head)
