64545be027d3cb26d25b1f8ebbe7e36330bab340
Zinebb12 <zinebzaid@gmail.com>
2023-10-26 19:15:46 -0700

    5. 101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - Number of bits that we need to flip
+ * @n: long int
+ * @m: unsigned int
+ * Return: numbr of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int fire = 0, wall;
+
+	wall = n ^ m;
+	while (wall > 0)
+	{
+		if (wall & 1)
+		{
+			fire++;
+		}
+		fire >>= 1;
+	}
+	return (fire);
+}
