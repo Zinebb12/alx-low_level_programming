a2236e7bd862f72d529ad26ea41408ecfe84abb1
Zinebb12 <zinebzaid@gmail.com>
2023-12-07 17:12:41 -0800

    Insert at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *insert_dnodeint_at_index - inserts a new node at a given position
+ *@h:head of the current DDL
+ *@idx:index the new node will be inserted
+ *@n:value of the new node
+ *Return:address of new_node or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = NULL;
+	unsigned int i = 0;
+	dlistint_t *current_node = *h;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (*h);
+	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	while (current_node != NULL)
+	{
+		if (i == idx)
+		{
+			new_node->next = current_node;
+			new_node->prev = current_node->prev;
+			current_node->prev->next = new_node;
+			current_node->prev = new_node;
+			return (new_node);
+		}
+		i++;
+		current_node = current_node->next;
+	}
+	/*end*/
+	if (i == idx)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	return (NULL);
+}
