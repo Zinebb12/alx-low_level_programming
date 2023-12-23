c670ba5f7fc8cddc577c9cb6d4ac738de4696de1
Zinebb12 <zinebzaid@gmail.com>
2023-12-22 16:25:53 -0800

    djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -20,6 +20,7 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
+
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -35,6 +36,7 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
