2088c56e365cbe23dde37c643b52cbdb5df6c338
Zinebb12 <zinebzaid@gmail.com>
2024-04-11 16:22:46 -0700

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
